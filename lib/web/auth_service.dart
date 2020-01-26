5433a5cd541a203326e4ac3372101efd27d00b73
Camara <victordsgnr@gmail.com>
2020-01-26 02:11:08 -0300

    buscando a api da heroku

--- lib/web/auth_service.dart
+++ lib/web/auth_service.dart
@@ -10,7 +10,7 @@ class AuthService {
     });
     
     var result = await http.post(
-      'https://6f0398e3.ngrok.io/api/signIn',         
+      'https://api-uni.herokuapp.com/api/signIn',         
       body: data,
       headers: {"Content-Type": "application/json"});
     
