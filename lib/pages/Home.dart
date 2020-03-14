c7f1625e1ff3d22d57ecd9f4595675bf067940b3
Camara <victor.camara@ksgestao.com.br>
2020-03-14 12:12:46 -0300

    criando widget dinamicamente

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:eni/components/heroPost.dart';
+import 'package:eni/controller/user.dart';
+import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,20 +15,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   int _currentIndex = 0;
   PageController _pageController;
+
+  var details;
+  Future<Response> startStore() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+    return await User().details(token);
+  }
 
   @override
   void initState() {
     super.initState();
+    startStore().then((result) {
+      setState(() {
+        details = json.decode(result.body);
+      });
+    });
     _pageController = PageController();
   }
+
   @override
   void dispose() {
     _pageController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     padding(component) {
@@ -39,11 +58,18 @@ class _HomePageState extends State<HomePage> {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Victor Camara', style: TextStyle(color: Colors.white, fontSize: 20.0),),
+            Text(
+              details != null ? details['username'] : '',
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
             SizedBox(
               height: 5.0,
             ),
-            Text('FullStack Developer at BrasilCap', style: TextStyle(color: Colors.white, fontSize: 12.0), textAlign: TextAlign.left,)
+            Text(
+              'FullStack Developer at BrasilCap',
+              style: TextStyle(color: Colors.white, fontSize: 12.0),
+              textAlign: TextAlign.left,
+            )
           ],
         ),
         CircleAvatar(
@@ -75,24 +101,28 @@ class _HomePageState extends State<HomePage> {
           margin: EdgeInsets.all(5.0),
           width: 100.0,
           child: InkWell(
-            onTap: () {
-              
-            },
+            onTap: () {},
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Icon(icon, color: Colors.white,),
-                SizedBox(height: 10.0,),
-                Text(text, style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800))
+                Icon(
+                  icon,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(text,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w800))
               ],
             ),
             borderRadius: BorderRadius.circular(10.0),
           ),
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(37, 116, 169, .5),
-          borderRadius: BorderRadius.circular(10),
-        )
-      );
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(37, 116, 169, .5),
+            borderRadius: BorderRadius.circular(10),
+          ));
     }
 
     Widget _allEvents() {
@@ -102,7 +132,7 @@ class _HomePageState extends State<HomePage> {
           Row(
             children: <Widget>[
               Text('All events', style: TextStyle(color: Colors.white)),
-            ],  
+            ],
           ),
           Container(
             height: 100.0,
@@ -120,101 +150,121 @@ class _HomePageState extends State<HomePage> {
       );
     }
 
-    Widget _cardEvent() {
+    Widget _cardEvent(item) {
       return InkWell(
-        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) {
-          return DetailScreen();
-        })),
-        child: Hero(
-          tag: "imageHero", 
-          child: Card(
-            semanticContainer: true,
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Eletronics', style: TextStyle(color: Colors.white),),
-                      SizedBox(height: 10.0,),
-                      Row(
+          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) {
+                return DetailScreen();
+              })),
+          child: Hero(
+              tag: item['id'],
+              child: Card(
+                semanticContainer: true,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Icon(Icons.schedule, size: 15.0, color: Colors.white,),
-                          SizedBox(width: 5.0,),
-                          Text('Jun 12 2020', style: TextStyle(color: Colors.white, fontSize: 10.0)),
+                          Text(
+                            item['title'],
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.schedule,
+                                size: 15.0,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              Text(item['date'],
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10.0)),
+                            ],
+                          ),
+                          Padding(
+                              padding: EdgeInsets.only(top: 5.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.place,
+                                    size: 15.0,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    width: 5.0,
+                                  ),
+                                  Text(item['locale'],
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 10.0))
+                                ],
+                              ))
                         ],
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 5.0),
-                        child: Row(
-                            children: <Widget>[
-                              Icon(Icons.place, size: 15.0, color: Colors.white,),
-                              SizedBox(width: 5.0,),
-                              Text('Alcântara', style: TextStyle(color: Colors.white, fontSize: 10.0))
-                            ],
-                          )
-                      )
-                    ],
-                  ),
+                    ),
+                    Image(
+                      image: AssetImage('images/eletronic.jpg'),
+                      width: 160.0,
+                    )
+                  ],
                 ),
-                Image(image: AssetImage('images/eletronic.jpg'), width: 160.0,)
-              ],
-            ),
-            color: Color.fromRGBO(37, 116, 169, .5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 5,
-            margin: EdgeInsets.only(left: 5.0, right: 5.0, top: 10.0, bottom: 10.0),
-          )
-        )
-      ); 
+                color: Color.fromRGBO(37, 116, 169, .5),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 5,
+                margin: EdgeInsets.only(
+                    left: 5.0, right: 5.0, top: 10.0, bottom: 10.0),
+              )));
     }
 
     Widget _popularEvents() {
-      return Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text('Popular events', style: TextStyle(color: Colors.white),)
-            ]
-          ),
-          Container(
+      return Column(children: <Widget>[
+        Row(children: <Widget>[
+          Text(
+            'Popular events',
+            style: TextStyle(color: Colors.white),
+          )
+        ]),
+        Container(
             width: double.infinity,
             height: 300.0,
-            child: ListView(
-              shrinkWrap: true,
-              padding: EdgeInsets.all(0),
-              scrollDirection: Axis.vertical,
-              children: <Widget>[
-                _cardEvent(),
-                // _cardEvent(),
-                // _cardEvent(),
-                // _cardEvent(),
-                // _cardEvent(),
-              ],
-            )
-          )
-        ]
-      );
+            child: ListView.builder(
+                itemCount: details['events'].length,
+                itemBuilder: (context, index) {
+                  return _cardEvent(details['events'][index]);
+                }))
+      ]);
     }
+
     Widget home() {
+      if (details == null) {
+        return SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 50.0,
+        );
+      }
       return SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              padding(profile),
-              padding(dataPicker()),
-              padding(_allEvents()),
-              padding(_popularEvents()),
-            ],
-          ),
-        )
-      );
+          child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            padding(profile),
+            padding(dataPicker()),
+            padding(_allEvents()),
+            padding(_popularEvents()),
+          ],
+        ),
+      ));
     }
+
     return Scaffold(
       backgroundColor: Color(0xFF102733),
       appBar: AppBar(
@@ -223,44 +273,52 @@ class _HomePageState extends State<HomePage> {
         actions: <Widget>[
           Padding(
             padding: EdgeInsets.only(right: 10.0),
-            child: Icon(Icons.notifications, color: Colors.white, size: 25.0,),
+            child: Icon(
+              Icons.notifications,
+              color: Colors.white,
+              size: 25.0,
+            ),
           ),
           Padding(
             padding: EdgeInsets.only(right: 10.0),
-            child: Icon(Icons.crop_free, color: Colors.white, size: 25.0,),
+            child: Icon(
+              Icons.crop_free,
+              color: Colors.white,
+              size: 25.0,
+            ),
           )
         ],
       ),
       drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
                 ),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
               ),
-              ListTile(
-                leading: Icon(Icons.message),
-                title: Text('Messages'),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('Profile'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-            ],
-          ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
       ),
       body: SizedBox.expand(
         child: PageView(
@@ -277,12 +335,13 @@ class _HomePageState extends State<HomePage> {
                 size: 150.0,
               ),
             ),
-            Container(color: Color(0xFF102733),),
+            Container(
+              color: Color(0xFF102733),
+            ),
             // Container(color: Color(0xFF102733),)
           ],
         ),
       ),
-
       bottomNavigationBar: BottomNavyBar(
         selectedIndex: _currentIndex,
         onItemSelected: (index) {
@@ -293,20 +352,36 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Color(0xFF102733),
         items: <BottomNavyBarItem>[
           BottomNavyBarItem(
-            title: Text('Home', style: TextStyle(color: _currentIndex == 0 ? Colors.yellow : Colors.white), textAlign: TextAlign.center,),
-            icon: Icon(Icons.home, color: _currentIndex == 0 ? Colors.yellow : Colors.white,)
-          ),
+              title: Text(
+                'Home',
+                style: TextStyle(
+                    color: _currentIndex == 0 ? Colors.yellow : Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              icon: Icon(
+                Icons.home,
+                color: _currentIndex == 0 ? Colors.yellow : Colors.white,
+              )),
           BottomNavyBarItem(
-            title: Text('Search', style: TextStyle(color: _currentIndex == 1 ? Colors.yellow : Colors.white), textAlign: TextAlign.center,),
-            icon: Icon(Icons.search, color: _currentIndex == 1 ? Colors.yellow : Colors.white)
-          ),
+              title: Text(
+                'Search',
+                style: TextStyle(
+                    color: _currentIndex == 1 ? Colors.yellow : Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              icon: Icon(Icons.search,
+                  color: _currentIndex == 1 ? Colors.yellow : Colors.white)),
           BottomNavyBarItem(
-            title: Text('Favorite', style: TextStyle(color: _currentIndex == 2 ? Colors.yellow : Colors.white), textAlign: TextAlign.center,),
-            icon: Icon(Icons.star, color: _currentIndex == 2 ? Colors.yellow : Colors.white)
-          ),
+              title: Text(
+                'Favorite',
+                style: TextStyle(
+                    color: _currentIndex == 2 ? Colors.yellow : Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              icon: Icon(Icons.star,
+                  color: _currentIndex == 2 ? Colors.yellow : Colors.white)),
         ],
       ),
     );
   }
 }
-
