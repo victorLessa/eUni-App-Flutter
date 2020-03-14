8ec695c2208a1f5ff9fae215d9dec54f415ef74c
Camara <victor.camara@ksgestao.com.br>
2020-03-14 12:12:14 -0300

    refactor autenticação

--- lib/web/auth_service.dart
+++ lib/web/auth_service.dart
@@ -2,18 +2,18 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+final baseUrl = 'https://api-uni.herokuapp.com';
+
 class AuthService {
   static Future<http.Response> loginUser(String email, String password) async {
     var data = jsonEncode({
       "email": email.trim().toLowerCase(),
       "password": password,
     });
-    
-    var result = await http.post(
-      'https://api-uni.herokuapp.com/api/signIn',         
-      body: data,
-      headers: {"Content-Type": "application/json"});
-    
-    return result; 
+
+    var result = await http.post('$baseUrl/signIn',
+        body: data, headers: {"Content-Type": "application/json"});
+
+    return result;
   }
 }
