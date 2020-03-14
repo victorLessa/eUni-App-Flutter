a73ad8cc0f141e7eb0f30ae736a1fe1ec7b0e782
Camara <victor.camara@ksgestao.com.br>
2020-03-14 12:11:14 -0300

    card de login

--- lib/components/cardLog.dart
+++ lib/components/cardLog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:eni/services/authenticate.dart' as Auth;
+import 'package:eni/controller/authenticate.dart' as Auth;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
@@ -9,7 +9,6 @@ class CardLogin extends StatefulWidget {
 }
 
 class CardLoginState extends State<CardLogin> {
-
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -18,72 +17,72 @@ class CardLoginState extends State<CardLogin> {
   void initState() {
     super.initState();
   }
+
   signIn(email, password) async {
     final result = await Auth.Authenticate().signIn(email, password);
     var body = json.decode(result.body);
-    if(result.statusCode == 200) {
+    if (result.statusCode == 200) {
       final prefs = await SharedPreferences.getInstance();
       await prefs.setString('token', body['token']);
+      await prefs.setString('details', result.body);
       Navigator.pushReplacementNamed(context, '/second');
       return;
     }
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(body['message']),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Fechar'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        );
-      }
-    );
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(body['message']),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Fechar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   Widget padding(component) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: component,
-    );;
+    );
+    ;
   }
 
-  Widget _email(){
+  Widget _email() {
     return TextFormField(
       controller: _emailController,
       decoration: InputDecoration(
-        labelText: 'E-mail',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(40.0),
-        ),
-        contentPadding: EdgeInsets.only(left: 30.0)
-      ),
+          labelText: 'E-mail',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          contentPadding: EdgeInsets.only(left: 30.0)),
       validator: (value) {
-        if(value.trim().isEmpty) {
+        if (value.trim().isEmpty) {
           return 'Insira uma senha válida';
         }
         return null;
       },
     );
   }
-  
+
   Widget _password() {
     return TextFormField(
       controller: _passwordController,
       obscureText: true,
       decoration: InputDecoration(
-        labelText: 'Senha',
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(40.0),
-        ),
-        contentPadding: EdgeInsets.only(left: 30.0)
-      ),
+          labelText: 'Senha',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          contentPadding: EdgeInsets.only(left: 30.0)),
       validator: (value) {
-        if(value.trim().isEmpty) {
+        if (value.trim().isEmpty) {
           return 'Insira um E-mail';
         }
         return null;
@@ -93,58 +92,56 @@ class CardLoginState extends State<CardLogin> {
 
   Widget _buttonLogin() {
     return SizedBox(
-      width: double.infinity,
-      child: FlatButton(
-
-        onPressed: () {
-          setState(() {
-            _textSubmit = 'Carregando...';
-          });
-          if(_formKey.currentState.validate()) {
-            signIn(_emailController.text, _passwordController.text); 
-          }
-          setState(() {
-            _textSubmit = 'Entrar';
-          });
-        },
-        child: Text(_textSubmit, style: TextStyle(color: Colors.white),),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0)
-        ),
-        color: Color.fromRGBO(37, 116, 169, .5),
-      )
-    );
+        width: double.infinity,
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              _textSubmit = 'Carregando...';
+            });
+            if (_formKey.currentState.validate()) {
+              signIn(_emailController.text, _passwordController.text);
+            }
+            setState(() {
+              _textSubmit = 'Entrar';
+            });
+          },
+          child: Text(
+            _textSubmit,
+            style: TextStyle(color: Colors.white),
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          color: Color.fromRGBO(37, 116, 169, .5),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Card(
-          margin: EdgeInsets.only(left: 50.0, right: 50.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    padding(_email()),
-                    padding(_password()),
-                    padding( _buttonLogin())
-                  ],
-                ),
-              )
-            ],
-          ),
-        )
-      )
-    );
+        child: Container(
+            child: Card(
+      margin: EdgeInsets.only(left: 50.0, right: 50.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                padding(_email()),
+                padding(_password()),
+                padding(_buttonLogin())
+              ],
+            ),
+          )
+        ],
+      ),
+    )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:eni/pages/Home.dart';
 import 'package:eni/pages/LoginV4.dart';
 
-void main () {
+void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: true,
     // Start the app with the "/" named route. In this case, the app starts
     // on the FirstScreen widget.
     initialRoute: '/',
     routes: {
-      // When navigating to the "/" route, build the FirstScreen widget.
       '/': (context) => Login(),
-      // When navigating to the "/second" route, build the SecondScreen widget.
       '/second': (context) => HomePage(),
     },
-    
-  )
-  ); 
+  ));
 }
-
-
